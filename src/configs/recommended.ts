215ea7dde8afa662e2f4f5db0b2169301dec37de
Aleksandar Grbic <ag@dreamdata.io>
2026-05-11 11:04:21 +0200

    initial commit

---
+++ src/configs/recommended.ts
@@ -0,0 +1,17 @@
+export const recommendedRules = {
+  "react-component-architecture/component-folder-structure": "error",
+  "react-component-architecture/index-must-reexport-default": "error",
+  "react-component-architecture/no-state-in-component-body": "warn",
+  "react-component-architecture/no-inline-jsx-functions": "error",
+  "react-component-architecture/no-jsx-computation": "warn",
+  "react-component-architecture/classnames-required": "error",
+  "react-component-architecture/classnames-import-name": "error",
+  "react-component-architecture/no-dark-mode-classes": "error",
+  "react-component-architecture/interface-prefix-i": "error",
+  "react-component-architecture/forwardref-display-name": "error",
+  "react-component-architecture/stories-require-default-export": "error",
+  "react-component-architecture/props-must-be-visual": "warn",
+  "react-component-architecture/react-import-named": "error",
+  "react-component-architecture/package-json-exact-deps": "error",
+  "react-component-architecture/github-actions-permissions": "error"
+} as const;
